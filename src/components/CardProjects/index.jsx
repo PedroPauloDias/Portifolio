0ed39bc2e6ab564ae7714ecfb31e54cb1a7d1a2a
unknown <pedroppld@gmail.com>
2023-05-06 16:30:33 -0300

    update button card project

--- src/components/CardProjects/index.jsx
+++ src/components/CardProjects/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import {GithubLogo , CaretDoubleRight} from '@phosphor-icons/react'
 
 export const CardProject = ({ imagem, alt, title, descricao }) => {
   return (
@@ -12,10 +13,11 @@ export const CardProject = ({ imagem, alt, title, descricao }) => {
           <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70 '></div>
 
           <div className='absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[60%] group-hover:translate-y-0 transition-all duration-500'>
-            <h2 className='font-dmserif text-3xl font-bold text-white  '> {title}</h2>
+            <h2 className='font-dmserif text-3xl font-bold text-white mb-2 '> {title}</h2>
             <p className='font-com text-lg italic text-white mb-4 opacity-0 group-hover:opacity-100  transition-opacity  duration-300'>{descricao}</p>
-            <div className=' flex justify-between'>
-          <button className='font-com rounded-full shadow shadow-black/60 bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white'>Saiba mais </button>
+            <div className=' flex justify-between gap-3 mt-2'>
+              <button className='font-com  flex justify-between items-center gap-2 rounded-full shadow shadow-black/60 bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:bg-teal-800 transition-transform duration-500 '> <GithubLogo size={18} /> Github </button>
+              <button className='font-com flex justify-between items-center gap-2   rounded-full shadow shadow-black/60 bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white hover:bg-teal-800  transition-transform duration-500 '> visitar <CaretDoubleRight size={18} /> </button>
             </div>
           </div>
         </div>
